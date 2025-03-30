a45d77b72a878945f8d485d8e569e5d226ecf6ee
samue <samuelgrossi40@gamil.com>
2025-03-30 12:27:43 +0100

    Creating main loop Connecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+from mountainShooter.code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(600, 480))
 
     def run(self, ):
-        pass
+        while True:
+            menu =Menu(self.window)
+            menu.run()
+
+
+
+
+       # while True:
+            # check all events
+        #    for event in pygame.event.get():
+         #       if event.type == pygame.QUIT:
+          #          pygame.quit() # close the window
+           #         quit() # end pygame
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-import pygame
+from mountainShooter.code.game import Game
 
-
-print('Start Setup')
-pygame.init()
-window = pygame.display.set_mode(size=(600, 480))
-
-print('End Setup')
-
-print('Loop Setup')
-while True:
-    # check all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() # close the window
-            quit() # end pygame
-
+game = Game()
+game.run()
